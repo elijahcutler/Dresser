6670c2a795a2f85e062a9f2a036dad3ef66a9b6f
Elijah <elijahcutler@gmail.com>
2018-03-06 12:45:24 -0500

    Preliminary setup, created project and added assets

---
+++ images/colors.swift
@@ -0,0 +1,8 @@
+let primaryColor = UIColor(red: 0.55, green: 0.43, blue: 0.39, alpha: 1.0);
+let primaryLightColor = UIColor(red: 0.75, green: 0.61, blue: 0.57, alpha: 1.0);
+let primaryDarkColor = UIColor(red: 0.37, green: 0.26, blue: 0.22, alpha: 1.0);
+let primaryTextColor = UIColor(red: 1.00, green: 1.00, blue: 1.00, alpha: 1.0);
+let secondaryColor = UIColor(red: 1.00, green: 0.80, blue: 0.74, alpha: 1.0);
+let secondaryLightColor = UIColor(red: 1.00, green: 1.00, blue: 0.93, alpha: 1.0);
+let secondaryDarkColor = UIColor(red: 0.80, green: 0.61, blue: 0.55, alpha: 1.0);
+let secondaryTextColor = UIColor(red: 0.00, green: 0.00, blue: 0.00, alpha: 1.0);
